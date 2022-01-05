f6a6b90f5a3314a44342cf99d24db3cc2988e0d1
Michal Bojanowski <michal2992@gmail.com>
2022-01-05 19:04:34 +0100

    Fix pooled ERGM estimation script
    Parallel prociessing based on R_CORES envvar.
    Use updated data file.
    Vertex attribute is called 'division' not 'div'.

--- ergm-pooled.R
+++ ergm-pooled.R
@@ -3,8 +3,14 @@ requireNamespace("igraph")
 library(ggplot2)
 library(ergm)
 library(ergm.count)
+box::use(
+  codebox/misc[get_r_cores]
+)
 
-load(here::here("data", "wgl.Rdata"))
+set.seed(666)
+r_cores <- get_r_cores()
+
+graphlist <- readRDS(here::here("data", "igraph-list.rds"))
 graphlist <- graphlist |>
   setNames(
     vapply(graphlist, igraph::graph_attr, character(1), name = "season")
@@ -24,7 +30,6 @@ season_from <- names(graphlist) |>
 bigg <- do.call(igraph::disjoint_union, graphlist)
 net <- intergraph::asNetwork(bigg)
 
-library(ergm.count)
 
 
 # Fit model 1 -------------------------------------------------------------
@@ -32,13 +37,13 @@ library(ergm.count)
 # All seasons/periods
 
 fit <- ergm(
-  net ~ nodefactor("season", levels = TRUE) + nodematch("div"),
+  net ~ nodefactor("season", levels = TRUE) + nodematch("division"),
   constraints = ~ blockdiag("season"),
   reference = ~ Poisson,
   response = "weight",
   verbose = TRUE,
   control = control.ergm(
-    parallel = 7
+    parallel = r_cores
   )
 )
 saveRDS(fit, file="ergm-pooled1.rds")
@@ -52,13 +57,13 @@ bigg2 <- do.call(igraph::disjoint_union, graphlist[season_from <= 2003])
 net2 <- intergraph::asNetwork(bigg2)
 
 fit2 <- ergm(
-  net2 ~ nodefactor("season", levels = TRUE) + nodematch("div"),
+  net2 ~ nodefactor("season", levels = TRUE) + nodematch("division"),
   constraints = ~ blockdiag("season"),
   reference = ~ Poisson,
   response = "weight",
   verbose = TRUE,
   control = control.ergm(
-    parallel = 7
+    parallel = r_cores
   )
 )
 
@@ -75,13 +80,13 @@ bigg3 <- do.call(igraph::disjoint_union, graphlist[season_from >= 2004])
 net3 <- intergraph::asNetwork(bigg3)
 
 fit3 <- ergm(
-  net3 ~ nodefactor("season", levels = TRUE) + nodematch("div"),
+  net3 ~ nodefactor("season", levels = TRUE) + nodematch("division"),
   constraints = ~ blockdiag("season"),
   reference = ~ Poisson,
   response = "weight",
   verbose = TRUE,
   control = control.ergm(
-    parallel = 7
+    parallel = r_cores
   )
 )
 
